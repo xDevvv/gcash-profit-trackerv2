56929279f43039832ca2f97ff8eb7782b3fa5c8f
xDevvv <zabsfreks@gmail.com>
2026-09-17 12:19:27 +0800

    Refactor fee routes to organize transfer, kload, and load fee management under distinct endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\KloadFeeController;
+use App\Http\Controllers\LoadFeeController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\TransferFeeController;
 use Illuminate\Support\Facades\Route;
@@ -17,7 +19,20 @@ Route::put('/transactions/{transaction}', [TransactionController::class, 'update
 Route::delete('/transactions/{transaction}', [TransactionController::class, 'destroy'])->name('transactions.destroy');
 Route::get('/transactions/preview-fee', [TransactionController::class, 'previewFee'])->name('transactions.previewFee');
 
-Route::get('/settings/fee-tiers', [TransferFeeController::class, 'index'])->name('transfer-fee.index');
-Route::post('/settings/fee-tiers', [TransferFeeController::class, 'store'])->name('transfer-fee.store');
-Route::put('/settings/fee-tiers/{transferFee}', [TransferFeeController::class, 'update'])->name('transfer-fee.update');
-Route::delete('/settings/fee-tiers/{transferFee}', [TransferFeeController::class, 'destroy'])->name('transfer-fee.destroy');
+// Transfer Fee Routes
+Route::get('/fee/transfer', [TransferFeeController::class, 'index'])->name('transfer.index');
+Route::post('/fee/transfer', [TransferFeeController::class, 'store'])->name('transfer.store');
+Route::put('/fee/transfer/{transferFee}', [TransferFeeController::class, 'update'])->name('transfer.update');
+Route::delete('/fee/transfer/{transferFee}', [TransferFeeController::class, 'destroy'])->name('transfer.destroy');
+
+// KLoad Fee Routes
+Route::get('/fee/kload', [KloadFeeController::class, 'index'])->name('kload.index');
+Route::post('/fee/kload', [KloadFeeController::class, 'store'])->name('kload.store');
+Route::put('/fee/kload/{kloadFee}', [KloadFeeController::class, 'update'])->name('kload.update');
+Route::delete('/fee/kload/{kloadFee}', [KloadFeeController::class, 'destroy'])->name('kload.destroy');
+
+// Load Fee Routes
+Route::get('/fee/load', [LoadFeeController::class, 'index'])->name('load.index');
+Route::post('/fee/load', [LoadFeeController::class, 'store'])->name('load.store');
+Route::put('/fee/load/{loadFee}', [LoadFeeController::class, 'update'])->name('load.update');
+Route::delete('/fee/load/{loadFee}', [LoadFeeController::class, 'destroy'])->name('load.destroy');
